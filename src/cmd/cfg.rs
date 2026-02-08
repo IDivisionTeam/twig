3235372266889c0fe169f100d165ac70e5ca161f
Yaroslav Ovdiienko <yaroslav.ovdiienko@gmail.com>
2026-02-08 10:08:59 +0100

    fix: prevent panic when config throws ConfigError on unwrap

--- src/cmd/cfg.rs
+++ src/cmd/cfg.rs
@@ -26,12 +26,14 @@ enum Commands {
 
 type Result<T> = std::result::Result<T, ConfigCmdError>;
 
-#[derive(Error, Debug, PartialEq, Eq)]
+#[derive(Error, Debug)]
 pub enum ConfigCmdError {
     #[error("failed to get value by key {0}")]
     InvalidKey(String),
     #[error("failed to deserialize value")]
     DeserializeValue,
+    #[error("failed to read config")]
+    ConfigError(#[from] config::ConfigError),
 }
 
 pub fn handle(args: &Cfg, config: &config::Config) -> Result<()> {
@@ -55,9 +57,8 @@ fn handle_list_cmd(config: &config::Config) {
 }
 
 fn handle_get_cmd(name: &str) -> Result<()> {
-    let value: Value = read_config_value(name)
-        .unwrap()
-        .ok_or_else(|| ConfigCmdError::InvalidKey(name.to_string()))?;
+    let value: Value = read_config_value(name)?
+        .ok_or(ConfigCmdError::InvalidKey(name.to_string()))?;
 
     let value_to_print: Option<String> = value.deserialize().ok();
     match value_to_print {

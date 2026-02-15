64a9024fd2c08fd86434f43b588d331fbe2ae746
Yaroslav Ovdiienko <yaroslav.ovdiienko@gmail.com>
2026-02-15 12:12:54 +0100

    refactor: handle all `Value` types gracefully

--- src/cmd/cfg.rs
+++ src/cmd/cfg.rs
@@ -1,7 +1,7 @@
 use crate::config;
 use crate::config::read_config_value;
 use clap::{Args, Subcommand};
-use figment::value::Value;
+use figment::value::{Empty, Value};
 use thiserror::Error;
 
 #[derive(Args)]
@@ -30,8 +30,8 @@ type Result<T> = std::result::Result<T, ConfigCmdError>;
 pub enum ConfigCmdError {
     #[error("failed to get value by key {0}")]
     InvalidKey(String),
-    #[error("failed to deserialize value")]
-    DeserializeValue,
+    #[error("failed to get any results")]
+    UnexpectedError,
     #[error("failed to read config")]
     ConfigError(#[from] config::ConfigError),
 }
@@ -57,15 +57,44 @@ fn handle_list_cmd(config: &config::Config) {
 }
 
 fn handle_get_cmd(name: &str) -> Result<()> {
-    let value: Value = read_config_value(name)?
-        .ok_or(ConfigCmdError::InvalidKey(name.to_string()))?;
+    let value: Value =
+        read_config_value(name)?.ok_or(ConfigCmdError::InvalidKey(name.to_string()))?;
 
-    let value_to_print: Option<String> = value.deserialize().ok();
+    let value_to_print = print_value(&value);
     match value_to_print {
         Some(v) => {
             println!("{}", v);
             Ok(())
         }
-        None => Err(ConfigCmdError::DeserializeValue),
+
+        None => Err(ConfigCmdError::UnexpectedError),
+    }
+}
+
+fn print_value(value: &Value) -> Option<String> {
+    match value {
+        Value::Empty(_, e) => match e {
+            Empty::None => Some("unspecified".to_string()),
+            Empty::Unit => None,
+        },
+        Value::Bool(_, b) => Some(b.to_string()),
+        Value::Num(_, n) => Some(n.to_actual().to_string()),
+        Value::String(_, s) => Some(s.clone()),
+        Value::Char(_, c) => Some(c.to_string()),
+        Value::Array(_, arr) => Some(
+            arr.iter()
+                .filter_map(print_value)
+                .collect::<Vec<_>>()
+                .join(", "),
+        ),
+        Value::Dict(_, map) => Some(
+            map.iter()
+                .filter_map(|(k, v)| {
+                    let value = print_value(v).unwrap_or(String::new());
+                    Some(format!("{} = {}", k, value))
+                })
+                .collect::<Vec<_>>()
+                .join("\n"),
+        ),
     }
 }

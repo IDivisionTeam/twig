8ba8f3efdac3bbedc0119e4d56a04b01ed31bffd
Yaroslav Ovdiienko <46504259+ardiien@users.noreply.github.com>
2026-02-15 18:50:24 +0100

    [BR-50] Port config get command (#12)

--- src/cmd/cfg.rs
+++ src/cmd/cfg.rs
@@ -1,6 +1,8 @@
 use crate::config;
-use anyhow::Result;
+use crate::config::read_config_value;
 use clap::{Args, Subcommand};
+use figment::value::{Empty, Value};
+use thiserror::Error;
 
 #[derive(Args)]
 pub struct Cfg {
@@ -12,6 +14,7 @@ pub struct Cfg {
 enum Commands {
     /// List all variables set in config file, along with their values.
     List,
+    /// Emits the value of the specified key.
     Get {
         name: String,
     },
@@ -21,13 +24,22 @@ enum Commands {
     },
 }
 
+type Result<T> = std::result::Result<T, ConfigCmdError>;
+
+#[derive(Error, Debug)]
+pub enum ConfigCmdError {
+    #[error("failed to get value by key {0}")]
+    InvalidKey(String),
+    #[error("failed to get any results")]
+    UnexpectedError,
+    #[error("failed to read config")]
+    ConfigError(#[from] config::ConfigError),
+}
+
 pub fn handle(args: &Cfg, config: &config::Config) -> Result<()> {
     match &args.command {
         Commands::List => handle_list_cmd(config),
-        #[allow(unused_variables)]
-        Commands::Get { name } => {
-            todo!()
-        }
+        Commands::Get { name } => handle_get_cmd(name)?,
         #[allow(unused_variables)]
         Commands::Set { name, value } => {
             todo!()
@@ -38,5 +50,45 @@ pub fn handle(args: &Cfg, config: &config::Config) -> Result<()> {
 }
 
 fn handle_list_cmd(config: &config::Config) {
-    print!("{}\n{}\n{}", config.credentials, config.project, config.mapping);
+    print!(
+        "{}\n{}\n{}",
+        config.credentials, config.project, config.mapping
+    );
+}
+
+fn handle_get_cmd(name: &str) -> Result<()> {
+    let value: Value =
+        read_config_value(name)?.ok_or(ConfigCmdError::InvalidKey(name.to_string()))?;
+
+    let value_to_print = print_value(&value).ok_or(ConfigCmdError::UnexpectedError)?;
+    println!("{value_to_print}");
+    Ok(())
+}
+
+fn print_value(value: &Value) -> Option<String> {
+    match value {
+        Value::Empty(_, e) => match e {
+            Empty::None => Some("unspecified".to_string()),
+            Empty::Unit => None,
+        },
+        Value::Bool(_, b) => Some(b.to_string()),
+        Value::Num(_, n) => Some(n.to_actual().to_string()),
+        Value::String(_, s) => Some(s.clone()),
+        Value::Char(_, c) => Some(c.to_string()),
+        Value::Array(_, arr) => Some(
+            arr.iter()
+                .filter_map(print_value)
+                .collect::<Vec<_>>()
+                .join(", "),
+        ),
+        Value::Dict(_, map) => Some(
+            map.iter()
+                .map(|(k, v)| {
+                    let value = print_value(v).unwrap_or_default();
+                    format!("{} = {}", k, value)
+                })
+                .collect::<Vec<_>>()
+                .join("\n"),
+        ),
+    }
 }
